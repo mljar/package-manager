6a6c9a5325377a5e3e530681466f4d02f0203c59
Piotr <pplonski86@gmail.com>
2025-02-19 12:35:46 +0100

    update package name

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ jlpm run build
 
 python -m build
 
-cp dist/package_manager-0.1.0-py3-none-any.whl ../studio/env_installer/extras/
+cp dist/jupyter_package_manager-*-py3-none-any.whl ../studio/env_installer/extras/
