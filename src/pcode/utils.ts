6ac53920d5ac5398497402dc3f12f674f4689fea
Piotr <pplonski86@gmail.com>
2025-04-07 13:19:01 +0200

    add __mljar prefix to python functions used to filter them out in variable explorer

--- src/pcode/utils.ts
+++ src/pcode/utils.ts
@@ -1,5 +1,5 @@
 export const listPackagesCode = `
-def __list_packages():
+def __mljar__list_packages():
     from importlib.metadata import distributions
     pkgs = []
     seen = set()
@@ -10,41 +10,38 @@ def __list_packages():
             pkgs.append({"name": name, "version": dist.version})
     return pkgs
 
-__list_packages()
+__mljar__list_packages()
 `;
 
-export const installPackagePip = (pkg: string): string =>
-  `
-def __install_pip():
+export const installPackagePip = (pkg: string): string => `
+def __mljar__install_pip():
     import subprocess
     import sys
     python_exe = sys.executable
     if python_exe.startswith('\\\\?'):
         python_exe = python_exe[4:] 
     subprocess.check_call([python_exe, '-m', 'pip', 'install', '${pkg}'])
-__install_pip()
+__mljar__install_pip()
 `;
 
-
-
 export const removePackagePip = (pkg: string): string => `
-def __remove_package():
+def __mljar__remove_package():
     import subprocess
     import sys
     python_exe = sys.executable
     if python_exe.startswith('\\\\?'):
         python_exe = python_exe[4:]
     subprocess.check_call([python_exe, '-m', 'pip', 'uninstall', '-y', '${pkg}'])
-__remove_package()
-`
+__mljar__remove_package()
+`;
 
 export const checkIfPackageInstalled = (pkg: string) => `
-def __check_if_installed():
+def __mljar__check_if_installed():
     from importlib.metadata import distributions
     for dist in distributions():
         if dist.metadata["Name"].lower() == "${pkg}".lower():
             print("INSTALLED")
             return
     print("NOT_INSTALLED")
-__check_if_installed()
-`
+__mljar__check_if_installed()
+`;
